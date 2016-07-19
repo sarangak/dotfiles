8ccbedfec05e025d557cd8e08c51b652d2b51450
Saranga Komanduri <skomanduri@civisanalytics.com>
2016-07-19 12:40:16 -0500

    Add Karabiner settings

---
+++ Customs/Karabiner/import-settings.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
+
+$cli set bilalh.remap.f19_escape_control 1
+/bin/echo -n .
+$cli set general.dont_remap_thirdvendor_pointing 1
+/bin/echo -n .
+$cli set parameter.keyoverlaidmodifier_timeout 300
+/bin/echo -n .
+$cli set private.lock_with_cmd_l 1
+/bin/echo -n .
+$cli set private.open_dev_tools_with_f12 1
+/bin/echo -n .
+$cli set private.refresh_with_f5 1
+/bin/echo -n .
+$cli set remap.control_delete_to_option_delete_winstyle 1
+/bin/echo -n .
+$cli set remap.copy_paste_winstyle_new_browser_tab 1
+/bin/echo -n .
+$cli set remap.find_winstyle_no_term 1
+/bin/echo -n .
+$cli set remap.new_winstyle_no_term 1
+/bin/echo -n .
+$cli set remap.option_tab_to_switch_application 1
+/bin/echo -n .
+$cli set remap.pc_style_copy_paste 1
+/bin/echo -n .
+$cli set remap.pclikecontrolleftright_except_vm 1
+/bin/echo -n .
+$cli set remap.pclikeshiftdelte_except_vm 1
+/bin/echo -n .
+$cli set remap.pcstyle_close 1
+/bin/echo -n .
+$cli set remap.redo_winstyle_no_term 1
+/bin/echo -n .
+$cli set remap.save_winstyle_no_term 1
+/bin/echo -n .
+$cli set remap.select_all_winstyle_no_term 1
+/bin/echo -n .
+$cli set remap.undo_winstyle_no_term 1
+/bin/echo -n .
+$cli set remap.vimnormal_ctrlbracketleft_toggle 1
+/bin/echo -n .
+$cli set repeat.initial_wait 416
+/bin/echo -n .
+$cli set repeat.wait 33
+/bin/echo -n .
+/bin/echo
