0499581ef017aa47676720eae319336fe542b5c0
Saranga Komanduri <skomanduri@civisanalytics.com>
2020-04-13 10:20:18 -0700

    Add "Insert a link" Slack shortcut

--- dot_hammerspoon/slack.lua
+++ dot_hammerspoon/slack.lua
@@ -3,25 +3,36 @@
 ]]
 local slackHotkeys = {
   hs.hotkey.new({'ctrl'}, '/', nil, function()
+      -- Search
       hs.eventtap.keyStroke({'cmd'}, 'k')
   end),
   hs.hotkey.new({'ctrl', 'shift'}, '/', nil, function()
+      -- Start a DM with multiple people
       hs.eventtap.keyStroke({'cmd', 'shift'}, 'k')
   end),
   hs.hotkey.new({'ctrl'}, 'h', nil, function()
+      -- Go back in channel history
       hs.eventtap.keyStroke({'cmd'}, '[')
   end),
   hs.hotkey.new({'ctrl'}, 'l', nil, function()
+      -- Go forward in channel history
       hs.eventtap.keyStroke({'cmd'}, ']')
   end),
   hs.hotkey.new({'ctrl'}, 'k', nil, function()
+      -- Go up in channel list
       hs.eventtap.keyStroke({'alt', 'shift'}, 'up')
   end),
+  hs.hotkey.new({'ctrl'}, 'j', nil, function()
+      -- Go down in channel list
+      hs.eventtap.keyStroke({'alt', 'shift'}, 'down')
+  end),
   hs.hotkey.new({'ctrl'}, 'u', nil, function()
+      -- Undo
       hs.eventtap.keyStroke({'cmd'}, 'up')
   end),
-  hs.hotkey.new({'ctrl'}, 'j', nil, function()
-      hs.eventtap.keyStroke({'alt', 'shift'}, 'down')
+  hs.hotkey.new({'cmd'}, 'k', nil, function()
+      -- Insert a link
+      hs.eventtap.keyStroke({'cmd', 'shift'}, 'u')
   end)
 }
 
