dcdccc2ab7d7d6ec41086d380a5c45a067c378f7
Saranga Komanduri <sarangak@cs.cmu.edu>
2022-12-15 12:28:02 -0700

    Try copilot and cue-mode packages

--- dot_emacs-profiles.el
+++ dot_emacs-profiles.el
@@ -1,5 +1,5 @@
 (("master" . ((user-emacs-directory . "~/.emacs.d"))) ;; Spacemacs master (for stability)
                                                        ;; managed by ~/.spacemacs file
- ("default" . ((user-emacs-directory . "~/.spacemacs.develop") ;; Spacemacs develop (for typescript)
+ ("default" . ((user-emacs-directory . "~/.spacemacs.develop") ;; Spacemacs develop (I basically only use this now)
                                                                ;; managed by ~/.spacemacs-develop-config/init.el
                (env . (("SPACEMACSDIR" . "~/.spacemacs-develop-config"))))))
